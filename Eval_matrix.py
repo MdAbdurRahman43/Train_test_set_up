df4dc57ea3992d2ff4542891773d81ffac829834
MdAbdurRahman43 <80150532+MdAbdurRahman43@users.noreply.github.com>
2025-08-28 17:01:46 +0600

    Update Eval_matrix.py

--- Eval_matrix.py
+++ Eval_matrix.py
@@ -2,6 +2,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+import torch
 class eval_matrix:
     def __init__(self,model):
         self.device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
